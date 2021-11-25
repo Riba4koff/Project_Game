22c44b53f46bb851a65e357764e2d91b73d5e0f2
Pavel <riba4koff@mail.ru>
2021-11-25 05:14:42 +0300

    add_win_moves_bot Добавлены выйгрышные ходы компьютеру

--- Project_Game/Project_Game.cpp
+++ Project_Game/Project_Game.cpp
@@ -8,6 +8,8 @@ using namespace std;
 //2 - 0 (нолик)
 
 
+
+//функция вывода поля
 void Show_arr(int arr[3][3], int SIZE)
 {
 	for (int i = 0; i < SIZE; i++)
@@ -20,6 +22,7 @@ void Show_arr(int arr[3][3], int SIZE)
 	}
 	cout << "\n";
 }
+//функция проверки победы
 bool WhoWin(int arr[3][3], int SIZE, int value)
 {
 	for (int i = 0; i < SIZE; i++)
@@ -41,6 +44,7 @@ bool WhoWin(int arr[3][3], int SIZE, int value)
 	}
 	return false;
 }
+//функция проверки ничьи
 bool is_Draw(int arr[3][3], int SIZE)
 {
 	for (int i = 0; i < SIZE; i++)
@@ -53,10 +57,10 @@ bool is_Draw(int arr[3][3], int SIZE)
 	}
 	return true;
 }
-
-
-void moves_computer(int arr[3][3], int SIZE)
+//функция невыгодных ходов компьютера
+void block_moves(int arr[3][3], bool& moves)
 {
+	const int SIZE = 3;
 	bool move = true; //move - проверкана то, был ли выполнен ход компьютером, если да, то move = false и ни в один ход он больше не зайдет.
 	int x = 0;
 	int y = 0;
@@ -152,6 +156,106 @@ void moves_computer(int arr[3][3], int SIZE)
 		arr[x][y] = 2;
 	}
 }
+//функция выгодных ходов компьютера
+void win_moves_computer(int arr[3][3], bool& moves)
+{
+	const int SIZE = 3;
+	bool move = true; //move - проверкана то, был ли выполнен ход компьютером, если да, то move = false и ни в один ход он больше не зайдет.
+	int x = 0;
+	int y = 0;
+
+	//Выгодный ход по горизонтали
+	if (move)
+	{
+		for (int y = 0; y < SIZE; y++)
+		{
+			if (arr[x][y] == 2 && arr[x + 1][y] == 2 && arr[x + 2][y] == 0)
+			{
+				arr[x + 2][y] = 2;
+				moves = move = false;
+			}
+			else if (arr[x][y] == 0 && arr[x + 1][y] == 2 && arr[x + 2][y] == 2)
+			{
+				arr[x][y] = 2;
+				moves = move = false;
+			}
+			else
+				if (arr[x][y] == 2 && arr[x + 1][y] == 0 && arr[x + 2][y] == 2)
+				{
+					arr[x + 1][y] = 2;
+					moves = move = false;
+				}
+		}
+	}
+	//выгодный ход по вертикали
+	if (move)
+	{
+		for (int x = 0; x < SIZE; x++)
+		{
+			if (arr[x][y] == 2 && arr[x][y + 1] == 2 && arr[x][y + 2] == 0)
+			{
+				arr[x][y + 2] = 2;
+				moves = move = false;
+			}
+			else if (arr[x][y] == 0 && arr[x][y + 1] == 2 && arr[x][y + 2] == 2)
+			{
+				arr[x][y] = 2;
+				moves = move = false;
+			}
+			else
+				if (arr[x][y] == 2 && arr[x][y + 1] == 0 && arr[x][y + 2] == 2)
+				{
+					arr[x][y + 1] = 2;
+					moves = move = false;
+				}
+
+		}
+	}
+	//выгодный ход по диагонали
+	if (move)
+	{
+		if (arr[0][0] == 2 && arr[1][1] == 0 && arr[2][2] == 2)
+		{
+			arr[1][1] = 2;
+			moves = move = false;
+		}
+		else if (arr[0][2] == 2 && arr[1][1] == 0 && arr[2][0] == 2)
+		{
+			arr[1][1] = 2;
+			moves = move = false;
+		}
+		else if (arr[0][0] == 2 && arr[1][1] == 2 && arr[2][2] == 0)
+		{
+			arr[2][2] = 2;
+			moves = move = false;
+		}
+		else if (arr[0][0] == 0 && arr[1][1] == 2 && arr[2][0] == 2)
+		{
+			arr[0][0] = 2;
+			moves = move = false;
+		}
+		else if (arr[2][0] == 2 && arr[1][1] == 2 && arr[0][2] == 0)
+		{
+			arr[0][2] = 2;
+			moves = move = false;
+		}
+		else
+			if (arr[0][2] == 2 && arr[1][1] == 2 && arr[2][0] == 0)
+			{
+				arr[2][0] = 2;
+				moves = move = false;
+			}
+	}
+}
+//функция всех ходов компьютера
+void moves_computer(int arr[3][3], int SIZE)
+{
+	bool moves = true;
+	win_moves_computer(arr, moves); //выгодные ходы для пк, если таковы есть, иначе идем к блокировке.
+	if (moves)
+		block_moves(arr, moves); //Блокировка выгодных ходов для человека
+
+}
 
 
 int main()
@@ -160,17 +264,20 @@ int main()
 	srand(time(NULL));
 
 	const int SIZE = 3;
-	//  x  y 
 	int arr[SIZE][SIZE]{};
 	int x = 0, y = 0;
 	bool flag = true;
 
+	cout << "Подсказка. x вправо --- 0 --- 1 --- 2 -- > \t y вниз --- 0 --- 1 --- 2 -- >\n";
 	Show_arr(arr, SIZE);
+
 
 	while (flag)
 	{
+		
 		cout << "Ваш ход!\nВведите координаты x y: ";
 		cin >> x >> y;
+		cout << "\n";
 		cout << "\n";
 
 		int temp = arr[x][y];
@@ -206,24 +313,22 @@ int main()
 
 		cout << "ход компьютера\n\n";
 
-
-
 		moves_computer(arr, SIZE);
 
 
 		Show_arr(arr, SIZE);
 
-		//if (WhoWin(arr, SIZE, 2)) //Функция проверки победы компьютера. Параметр 2 - это нолик, т.е. ход компьютера
-		//{
-		//	cout << "Вы проиграли\n";
-		//	break;
-		//}
+		if (WhoWin(arr, SIZE, 2)) //Функция проверки победы компьютера. Параметр 2 - это нолик, т.е. ход компьютера
+		{
+			cout << "Вы проиграли\n";
+			break;
+		}
 
-		//if (is_Draw(arr, SIZE))
-		//{
-		//	cout << "Ничья!\n\n";
-		//	break;
-		//}
+		if (is_Draw(arr, SIZE))
+		{
+			cout << "Ничья!\n\n";
+			break;
+		}
 
 
 	}
